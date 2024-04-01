a0b761f614c2025cef47937700b2a9ba49e54787
bismansahni <bismansahni@outlook.com>
2024-04-01 15:33:24 -0700

    changed the skill names section

--- src/Skill.js
+++ src/Skill.js
@@ -4,6 +4,7 @@ import React from 'react';
 import awsLogo from './images/awslogo.png';
 import sqllogo from './images/sqllogo.png';
 import mongologo from './images/mongologo.png';
+import pythonlogo from './images/pythonlogo.png';
 
 import './App.css';
 
@@ -14,12 +15,14 @@ function Skills() {
         <div className="skills-container">
           <div className="skill-card">
             <img src={awsLogo} alt="AWS" className="skill-icon" />
-            <div className="skill-name">AWS</div>
+            {/* <div className="skill-name">AWS</div> */}
 
             <img src={sqllogo} alt="sql" className="skill-icon" />
-            <div className="skill-name">sql</div>
+            {/* <div className="skill-name">sql</div> */}
             <img src={mongologo} alt="mongodb" className="skill-icon" />
-            <div className="skill-name">hello</div>
+            {/* <div className="skill-name">mongodb</div> */}
+            <img src={pythonlogo} alt="python" className="skill-icon" />
+            {/* <div className="skill-name">python</div> */}
           </div>
           {/* Repeat for other skill cards */}
         </div>
