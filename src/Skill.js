d61ac4cc5413167eb318604dc3f6033e6d114dc8
bismansahni <bismansahni@outlook.com>
2024-04-01 22:49:11 -0700

    updated skills area

--- src/Skill.js
+++ src/Skill.js
@@ -1,33 +1,40 @@
-// Skills.js
-
 import React from 'react';
 import awsLogo from './images/awslogo.png';
 import sqllogo from './images/sqllogo.png';
 import mongologo from './images/mongologo.png';
 import pythonlogo from './images/pythonlogo.png';
-
+import javalogo from './images/javalogo.png';
+import clogo from './images/clogo.png'
 import './App.css';
- import './style/Skill.css'
+import './style/Skill.css';
 
 function Skills() {
     return (
-      <div className="skills-section">
-        <h2 className="skills-title">Skills</h2>
-        <div className="skills-container">
-          <div className="skill-card">
-            <img src={awsLogo} alt="AWS" className="skill-icon" />
-            {/* <div className="skill-name">AWS</div> */}
-
-            <img src={sqllogo} alt="sql" className="skill-icon" />
-            {/* <div className="skill-name">sql</div> */}
-            <img src={mongologo} alt="mongodb" className="skill-icon" />
-            {/* <div className="skill-name">mongodb</div> */}
-            <img src={pythonlogo} alt="python" className="skill-icon" />
-            {/* <div className="skill-name">python</div> */}
-          </div>
-          {/* Repeat for other skill cards */}
+        <div className="skills-section">
+            <h2 className="skills-title">Skills</h2>
+            <div className="skills-container">
+                {/* Each skill-card div contains one skill-icon image */}
+                <div className="skill-card">
+                    <img src={awsLogo} alt="AWS" className="skill-icon" />
+                </div>
+                <div className="skill-card">
+                    <img src={sqllogo} alt="SQL" className="skill-icon" />
+                </div>
+                <div className="skill-card">
+                    <img src={mongologo} alt="MongoDB" className="skill-icon" />
+                </div>
+                <div className="skill-card">
+                    <img src={pythonlogo} alt="Python" className="skill-icon" />
+                </div>
+                <div className="skill-card">
+                    <img src={javalogo} alt="Java" className="skill-icon" />
+                </div>
+                <div className="skill-card">
+                    <img src={clogo} alt="C" className="skill-icon" />
+            </div>
         </div>
-      </div>
+        </div>
     );
-  }
+}
+
 export default Skills;
