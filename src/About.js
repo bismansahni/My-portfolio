28e25a422d7fb87aa45dfeab3fd7382fc8b55e33
bismansahniii <bismansahniii@gmail.com>
2024-04-23 06:53:12 +0000

    added animation to intro

--- src/About.js
+++ src/About.js
@@ -1,10 +1,8 @@
-
-
-
 import React from 'react';
-import bismanphoto from './images/bismanphoto.png'; // Adjust if necessary
-import githubIcon from './images/github.svg'; // Path to your GitHub icon
-import linkedInIcon from './images/linkedin.png'; // Path to your LinkedIn icon
+import { motion } from 'framer-motion';
+import bismanphoto from './images/bismanphoto.png';
+import githubIcon from './images/github.svg';
+import linkedInIcon from './images/linkedin.png';
 import './styles/About.css';
 
 function About() {
@@ -14,10 +12,17 @@ function About() {
         
         <img src={bismanphoto} alt="Bisman Sahni" className="about-photo" />
         <div className="about-text">
-          <h1>Hey, I am Bisman!</h1>
+          <motion.h1
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.5 }}
+          >
+            Hey, I am Bisman!
+          </motion.h1>
           <p>
-           A Computer Science major at Arizona State University specializing in Cybersecurity. My expertise spans several programming languages including Python, C++, and JavaScript, with a keen interest in augmented and virtual reality technologies. I am dedicated to exploring how advanced technologies can solve complex problems and improve everyday life. My passion for innovation is reflected in my involvement in various tech-driven projects, where I consistently strive to push the boundaries of what's possible with cutting-edge solutions.
+          A Computer Science major at Arizona State University.... 
           </p>
+          {/* specializing in Cybersecurity. My expertise spans several programming languages including Python, C++, and JavaScript, with a keen interest in augmented and virtual reality technologies. I am dedicated to exploring how advanced technologies can solve complex problems and improve everyday life. My passion for innovation is reflected in my involvement in various tech-driven projects, where I consistently strive to push the boundaries of what's possible with cutting-edge solutions. */}
         </div>
         <div className="social-icons">
           <a href="https://github.com/bismansahni" target="_blank" rel="noopener noreferrer">
