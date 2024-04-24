5caca80fd18137e5c48430bf00580158251f7b3d
bismansahniii <bismansahniii@gmail.com>
2024-04-24 04:24:57 +0000

    increased font size a bit

--- src/Navbar.js
+++ src/Navbar.js
@@ -25,12 +25,13 @@
   
 import React from 'react';
 import { resumeURL } from './Resume'; 
+import './styles/Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <a href="#about" className="nav-link"  style={{ color: '#16f2b3', fontSize: '24px' }}>
+        <a href="#about" className="nav-link" style={{ color: '#16f2b3', fontSize: '24px' }}>
           Bisman Sahni
         </a>
         <div>
@@ -38,12 +39,11 @@ function Navbar() {
           <a className="nav-link" href="#experience">EXPERIENCE</a>
           <a className="nav-link" href="#skills">SKILLS</a>
           <a className="nav-link" href="#education">EDUCATION</a>
-          <a className="nav-link" href={resumeURL} target="_blank" rel="noopener noreferrer">RESUME</a> 
+          <a className="nav-link" href={resumeURL} target="_blank" rel="noopener noreferrer">RESUME</a>
           <a className="nav-link" href="#projects">PROJECTS</a>
         </div>
       </div>
     </nav>
   );
 }
-
 export default Navbar;
