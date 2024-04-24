7dd4406d0ebf9c8f116cd798e89d4c2d38e19227
bismansahni <bismansahni@outlook.com>
2024-04-23 19:09:19 -0700

    Revert "added animation to the navbar"
    This reverts commit 5de22d1f7a3de586a249ff82c2960b2f682b3fe7.

--- src/Navbar.js
+++ src/Navbar.js
@@ -25,13 +25,12 @@
   
 import React from 'react';
 import { resumeURL } from './Resume'; 
-import './styles/Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <a href="#about" className="nav-link" style={{ color: '#16f2b3', fontSize: '24px' }}>
+        <a href="#about" className="nav-link"  style={{ color: '#16f2b3', fontSize: '24px' }}>
           Bisman Sahni
         </a>
         <div>
@@ -39,13 +38,12 @@ function Navbar() {
           <a className="nav-link" href="#experience">EXPERIENCE</a>
           <a className="nav-link" href="#skills">SKILLS</a>
           <a className="nav-link" href="#education">EDUCATION</a>
-          <a className="nav-link" href={resumeURL} target="_blank" rel="noopener noreferrer">RESUME</a>
+          <a className="nav-link" href={resumeURL} target="_blank" rel="noopener noreferrer">RESUME</a> 
           <a className="nav-link" href="#projects">PROJECTS</a>
         </div>
       </div>
     </nav>
   );
 }
-
 
 export default Navbar;
